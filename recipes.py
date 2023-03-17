8c93e4b59945db72a57bad17d5df34ac37f46ee8
taylor-5724 <113221147+taylor-5724@users.noreply.github.com>
2023-03-17 07:51:08 -0500

    Added setdir functionality

--- recipes.py
+++ recipes.py
@@ -5,37 +5,49 @@ def add_recipe():
     print('adding')
 
 def search_recipes():
-    print(options.search_input)
+    print(user_input.search_input)
 
 def list_recipes():
     print('listing')
 
 def set_dir():
-    print(options.setdir_input)
+    global directory
+    directory = user_input.setdir_input
+    print('save directory set to: ' + user_input.setdir_input)
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
 
-parser_add = subparsers.add_parser('add', help='add recipe to recipe book')
-parser_add.set_defaults(func=add_recipe)
-   
-parser_search = subparsers.add_parser('search', help='search recipes')
-parser_search.add_argument('search_input', type=str)
-parser_search.set_defaults(func=search_recipes)
+def user_input_get():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
 
-parser_search = subparsers.add_parser('list', help='list all recipes')
-parser_search.set_defaults(func=list_recipes)
+    parser_add = subparsers.add_parser('add', help='add recipe to recipe book')
+    parser_add.set_defaults(command=add_recipe)
+    
+    parser_search = subparsers.add_parser('search', help='search recipes')
+    parser_search.add_argument('search_input', type=str)
+    parser_search.set_defaults(command=search_recipes)
 
-parser_set_dir = subparsers.add_parser('setdir', help='set save directory')
-parser_set_dir.add_argument('setdir_input', type=str)
-parser_set_dir.set_defaults(func=set_dir)
+    parser_search = subparsers.add_parser('list', help='list all recipes')
+    parser_search.set_defaults(command=list_recipes)
 
-if len(sys.argv) <= 1:
-    sys.argv.append('--help')
+    parser_set_dir = subparsers.add_parser('setdir', help='set save directory')
+    parser_set_dir.add_argument('setdir_input', type=str)
+    parser_set_dir.set_defaults(command=set_dir)
 
-options = parser.parse_args()
+    #displays help if no arguments are added
+    if len(sys.argv) <= 1:
+        sys.argv.append('--help')
 
-options.func()
+    global user_input
+
+    user_input = parser.parse_args()
+
+
+user_input_get()
+user_input.command()
+
+
+
 
 
 
