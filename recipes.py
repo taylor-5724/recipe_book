d709baa4074cc6b81e9a6a70ab8e0342c8e903b2
taylor-5724 <113221147+taylor-5724@users.noreply.github.com>
2023-03-16 20:28:48 -0500

    Improved the search interface and renamed main.py

---
+++ recipes.py
@@ -0,0 +1,95 @@
+import argparse
+import sys
+
+def add_recipe():
+    print('adding')
+
+def search_recipes():
+    print(options.search_input)
+
+def list_recipes():
+    print('listing')
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+parser_add = subparsers.add_parser('add', help='add recipe to recipe book')
+parser_add.set_defaults(func=add_recipe)
+   
+parser_search = subparsers.add_parser('search', help='search recipes')
+parser_search.add_argument('search_input', type=str)
+parser_search.set_defaults(func=search_recipes)
+
+parser_search = subparsers.add_parser('list', help='list all recipes')
+parser_search.set_defaults(func=list_recipes)
+
+if len(sys.argv) <= 1:
+    sys.argv.append('--help')
+
+options = parser.parse_args()
+
+options.func()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
