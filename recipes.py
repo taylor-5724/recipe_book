943cf799d1af2ca0239a875b5d1f432fe5b1379e
taylor-5724 <113221147+taylor-5724@users.noreply.github.com>
2023-03-17 07:10:07 -0500

    Added setdir command

--- recipes.py
+++ recipes.py
@@ -10,6 +10,9 @@ def search_recipes():
 def list_recipes():
     print('listing')
 
+def set_dir():
+    print(options.setdir_input)
+
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
@@ -22,6 +25,10 @@ parser_search.set_defaults(func=search_recipes)
 
 parser_search = subparsers.add_parser('list', help='list all recipes')
 parser_search.set_defaults(func=list_recipes)
+
+parser_set_dir = subparsers.add_parser('setdir', help='set save directory')
+parser_set_dir.add_argument('setdir_input', type=str)
+parser_set_dir.set_defaults(func=set_dir)
 
 if len(sys.argv) <= 1:
     sys.argv.append('--help')
